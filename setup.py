3d8a98ee68b51b7e003571ee80be838f22518bd2
guptanuj890 <guptanuj890@gmail.com>
2025-05-25 12:31:03 +0530

    Logging and Exception added.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Anuj Gupta",
+    author_email="guptanuj890@gmail.com",
+    packages=find_packages()
+)
