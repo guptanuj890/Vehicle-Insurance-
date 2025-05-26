bde3225d45fa1b3a59fa7ec42acd1c38fc867af8
guptanuj890 <guptanuj890@gmail.com>
2025-05-26 19:32:14 +0530

    Data_Ingestion and Data_Transformation components added

---
+++ src/constants/__init__.py
@@ -0,0 +1,43 @@
+import os
+from datetime import date
+
+#for MongoDB connection
+DATABASE_NAME = "Proj1"
+COLLECTION_NAME = "Proj1-Data"
+MONGODB_URL_KEY = "MONGODB_URL"
+
+PIPELINE_NAME: str = ""
+ARTIFACT_DIR: str = "artifact"
+
+TARGET_COLUMN = "Response"
+CURRENT_YEAR = date.today().year
+
+FILE_NAME: str = "data.csv"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "Proj1-Data"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT: float = 0.25
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_REPORT_FILE_NAME: str = "report.yaml"
+
+"""
+Data Transformation ralated constant start with DATA_TRANSFORMATION VAR NAME
+"""
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+APP_HOST = "0.0.0.0"
+APP_PORT = 5000
