d149c42ddf1a42c37bcc6e22941d0b7d4356b87d
guptanuj890 <guptanuj890@gmail.com>
2025-05-26 20:03:37 +0530

    Model_training component added.

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -9,8 +9,11 @@ MONGODB_URL_KEY = "MONGODB_URL"
 PIPELINE_NAME: str = ""
 ARTIFACT_DIR: str = "artifact"
 
+MODEL_FILE_NAME = "model.pkl"
+
 TARGET_COLUMN = "Response"
 CURRENT_YEAR = date.today().year
+PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 
 FILE_NAME: str = "data.csv"
 TRAIN_FILE_NAME: str = "train.csv"
@@ -38,6 +41,21 @@ Data Transformation ralated constant start with DATA_TRANSFORMATION VAR NAME
 DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+"""
+MODEL TRAINER related constant start with MODEL_TRAINER var name
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
+MODEL_TRAINER_N_ESTIMATORS=200
+MODEL_TRAINER_MIN_SAMPLES_SPLIT: int = 7
+MODEL_TRAINER_MIN_SAMPLES_LEAF: int = 6
+MIN_SAMPLES_SPLIT_MAX_DEPTH: int = 10
+MIN_SAMPLES_SPLIT_CRITERION: str = 'entropy'
+MIN_SAMPLES_SPLIT_RANDOM_STATE: int = 101
 
 APP_HOST = "0.0.0.0"
 APP_PORT = 5000
